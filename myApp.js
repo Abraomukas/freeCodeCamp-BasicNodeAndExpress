aff4cbba9430659c770e2056039445dbead3a989
Abraomukas <abraomukas@gmail.com>
2022-07-18 10:37:39 +0400

    feat: added response + redirect

--- myApp.js
+++ myApp.js
@@ -6,10 +6,15 @@ console.log('Hello World');
 let indexPath = __dirname + '/views/index.html';
 let publicPath = __dirname + '/public';
 
-app.use("/public", express.static(publicPath));
+app.use('/public', express.static(publicPath));
 
 app.get('/', (_req, res) => {
 	res.sendFile(indexPath);
+});
+
+app.use('/json', (req, res) => {
+	res.json({ message: 'Hello json' });
+	res.redirect(301, req.url + '/json');
 });
 
 module.exports = app;
